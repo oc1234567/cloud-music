ff051d11646a537cc06b653c85fe5388176c6d89
yjzhang <yjzhang@Ctrip.com>
2020-08-25 23:09:37 +0800

    第八小节-从零开发数据层

---
+++ src/api/config.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export const baseUrl = "http://";
+
+const axiosInstance = axios.create({
+    baseUrl: baseUrl
+});
+
+axiosInstance.interceptors.response.use(
+    res => res.data,
+    err => {
+        console.log(err, "网络错误");
+    }
+);
+
+export {
+    axiosInstance
+};
